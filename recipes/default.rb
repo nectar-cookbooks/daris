1722ca16a27b245b8f8c38ca511303d63fa816a2
Stephen Crawley <s.crawley@uq.edu.au>
2013-10-14 16:53:35 +1000

    Fix

--- recipes/default.rb
+++ recipes/default.rb
@@ -48,7 +48,8 @@ end
 pkgs.each() do | pkg, file | 
   bash "fetch-#{pkg}" do
     user mflux_user
-    code "wget --user=#{user} --password=#{password} -O #{installers}/#{file} #{url}/#{file}"
+    code "wget --user=#{user} --password=#{password} --no-check-certificate " +
+         "-O #{installers}/#{file} #{url}/#{file}"
   not_if { ::File.exists?("#{installers}/#{file}") }
   end
 end 
