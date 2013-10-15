83b5fad39e722c45bd8ad3549aeff0e3e0ce4f11
Stephen Crawley <s.crawley@uq.edu.au>
2013-10-15 13:19:46 +1000

    Integrate build / install of 'pvconv'

--- metadata.rb
+++ metadata.rb
@@ -9,3 +9,4 @@ version "0.0.1"
 
 depends "java", ">= 1.13.0"
 depends "mediaflux"
+depends "pvconv"
--- recipes/default.rb
+++ recipes/default.rb
@@ -27,7 +27,9 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-include_recipe "daris"
+if ! ::File.exists?( '/usr/local/bin/pvconv.pl' ) then
+  include_recipe "pvconv"
+end
 
 mflux_home = node['mediaflux']['home']
 mflux_user = node['mediaflux']['user']
