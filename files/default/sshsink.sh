d2897bf5ef646233622e10b83e0e27eccac55019
Stephen Crawley <s.crawley@uq.edu.au>
2014-04-11 16:22:57 +1000

    Working on the sink script ...

--- files/default/sshsink.sh
+++ files/default/sshsink.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+#
+# Admin command for configuring Mediaflux / DaRIS sinks.
 
 . /etc/mediaflux/servicerc
 if [ $? -ne 0 ]; then
@@ -6,22 +8,77 @@ if [ $? -ne 0 ]; then
 fi
 
 MFCOMMAND=${MFLUX_BIN}/mfcommand
-
-#
-# Utility for configuring DaRIS SSH sinks.
-#
+RC=0
 
 addsink() {
+    NAME=$1
+    TYPE=scp
+    PORT=22
+    DESC=
+    DECOMP=true
+    FILEMODE=600
+    shift 2
+    while [ ?# -gt 0 ] ; do
+	case $1 in
+	    --host)
+		HOST=$2
+		shift 2
+		;;
+	    --port)
+		PORT=$2
+		shift 2
+		;;
+	    --desc*)
+		DESC="$2"
+		shift 2
+		;;
+	    --decomp*)
+		DECOMP=true
+		shift
+		;;
+	    --nodecomp*)
+		DECOMP=false
+		shift
+		;;
+	    --hostkey)
+		HOSTKEY=$2
+		shift 2
+		;;
+	    --filemode)
+		FILEMODE=$2
+		shift 2
+		;;
+	    --user)
+		USER=$2
+		shift 2
+		;;
+	    --password)
+		PASSWORD=$2
+		shift 2
+		;;
+	    --pkfile
+	        PKFILE=$2
+		shift 2
+		;;
+	    --*)
+		echo "unknown option $1"
+		RC=1
+		exit
+		;;
+	    
+	esac
+    done
     SCRIPT=/tmp/sshsink_$$
     cat > $SCRIPT <<EOF
-        sink.add :name $1 \
+        sink.add :name "$NAME" \
 	    :destination < \
-                :type scp :arg -name host $2 \
-	        :arg -name port 22 \
+                :type "$TYPE" :arg -name host "$HOST" \
+	        :arg -name port "$PORT" \
                 :arg -name hostkey [xvalue host-key [ssh.host.key.scan \
-                                                     :host $2 :type rsa]] \
-                :arg -name decompress true \
-            >  
+                                                     :host "$HOST" :type rsa]] \
+                :arg -name decompress "$DECOMP" \
+            > \
+            :description "$DESC"
 EOF
     $MFCOMMAND logon $MFLUX_DOMAIN $MFLUX_USER $MFLUX_PASSWORD
     $MFCOMMAND source $SCRIPT
@@ -52,8 +109,15 @@ describesink() {
 }
 
 help() {
-    echo "not implemented yet"
-    exit 1
+    echo "Usage: $1 subcommand [<args>]"
+    echo "where the subcommands are:"
+    echo "  add <sinkname> --host <hostname> [ --port <port> ]"
+    echo "               [ --description <description> ]" 
+    echo "               [ --decompress | --nodecompress ]" 
+    echo "                      - adds an SSH/SCP sink"
+    echo "  describe <sinkname> - describes a sink"
+    echo "  list                - lists all registered sink names"
+    echo "  remove <sinkname>   - removes a sink"
 }
 
 case $1 in
@@ -74,8 +138,8 @@ case $1 in
     describesink "$@"
     ;;
   help)
-    shift
-    help "$@"
+    CMD=$0
+    help "$0"
     ;;
   *)
     echo "Unknown subcommand '$1' - run '$0 help' for help"
