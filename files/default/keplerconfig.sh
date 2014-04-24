712161c71b8f6f2c0146098ca93db94101b8a32a
Stephen Crawley <s.crawley@uq.edu.au>
2014-04-24 12:45:23 +1000

    Fleshed out the 'method' subcommand (first cut)

--- files/default/keplerconfig.sh
+++ files/default/keplerconfig.sh
@@ -76,12 +76,12 @@ workflow() {
        NAME=`basename $WF .kar`
    fi
    cat >> $SCRIPT <<EOF
-       set uid [xvalue //*\[@name='SNAME'\]/@uid \
-                       [transform.definition.list]]
+       set uid [xvalue //*\[@name='$NAME'\]/@uid \
+                    [transform.definition.list]]
        if { [ string length \$uid ] == 0} {
-           transform.definition.create :type kepler :name $NAME :in file:$WF
+           transform.definition.create :type kepler :name \"$NAME\" :in file:$WF
        } else {
-           transform.definition.update :uid \$uid :name $NAME :in file:$WF
+           transform.definition.update :uid \$uid :name \"$NAME\" :in file:$WF
        }
 EOF
 
@@ -90,6 +90,46 @@ EOF
     RC=$?
     $MFCOMMAND logoff
     rm $SCRIPT
+}
+
+method() {
+    if [ $# -lt 1 ] ; then
+	echo "syntax: $0 method <name> [ <workflow> ... ] "
+	RC=1
+	exit
+    fi
+    NAME=$1
+    shift
+    SCRIPT=/tmp/keplerconfig_$$
+    SCRIPT_2=/tmp/keplerconfig_$$_2
+    NOS_WORKFLOWS=$#
+    rm -f $SCRIPT $SCRIPT_2
+    while [ $# -gt 0 ] ; do
+	WF_NAME=$1
+	shift
+	cat >> $SCRIPT <<EOF
+        set wf_${WF_NAME}_uid [xvalue //*\[@name='$WF_NAME'\]/@uid \
+                                  [transform.definition.list]]
+EOF
+	cat >> $SCRIPT_2 <<EOF
+            :step < \
+                :name \"$WF_NAME\" \
+                :transform < \
+                    :definition \$wf_${WF_NAME}_uid \
+                > /
+            > /
+EOF
+    done
+    cat >> $SCRIPT <<EOF
+        om.pssd.method.create :name \"$NAME\" :namespace "/pssd/methods" \
+EOF
+    cat >> $SCRIPT < $SCRIPT_2
+
+    $MFCOMMAND logon $MFLUX_DOMAIN $MFLUX_USER $MFLUX_PASSWORD
+    $MFCOMMAND source $SCRIPT
+    RC=$?
+    $MFCOMMAND logoff
+    rm $SCRIPT $SCRIPT_2
 }
 
 help() {
