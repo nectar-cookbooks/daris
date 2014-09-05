701c4c31658d41bc891f04770826aa0b04e54def
Stephen Crawley <s.crawley@uq.edu.au>
2014-09-05 14:46:11 +1000

    Deal with component name inconsistency when extracting the version nos

--- libraries/daris_urls.rb
+++ libraries/daris_urls.rb
@@ -312,11 +312,14 @@ module DarisUrls
                       ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE)
     release = { 'type' => type }
     urls.each do |key, url|
-      m = /.+-([0-9.]+)-mf([0-9.]+)-.*/.match(url)
+      # Note that the following regexes don't insist on specific stuff 
+      # (e.g. '-'s) before or after the version numbers, because the
+      # DaRIS component filenames are a bit inconsistent.
+      m = /.+([0-9.]+)-mf([0-9.]+).*/.match(url)
       if m then
         release[key] = [m[1], m[2]]
       else
-        m = /.+-([0-9.]+)-.*/.match(url)
+        m = /.+([0-9.]+).*/.match(url)
         if m then 
           release[key] = [m[1]]
         end
